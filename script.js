5dc8feffe63c4d4bb40825e0304f13cd97f9ea87
SinanFischer <sinanfischer@gmail.com>
2022-09-26 21:00:08 +0200

    small improvments

--- script.js
+++ script.js
@@ -21,7 +21,6 @@ async function includeHTML() {
 }
 
 function showAddTask() {
-
     document.getElementById('render').innerHTML = '<div w3-include-html="add_task.html"></div>';
     includeHTML();
     }
