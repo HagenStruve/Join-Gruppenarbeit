7252f9397c4f527aa5a97e421484e40e8bdb2b8d
matthiasmulzet <mulzetmatthias@gmail.com>
2022-09-29 20:28:34 +0200

    created sign_up.html

--- js/login.js
+++ js/login.js
@@ -2,45 +2,15 @@ let users = []
 
 
 function showSignUp() {
-    removeValueSignUpInput();
-    defaultViewSignUp();
-    loginDisappear();
-    document.getElementById('password').classList.remove('no-margin-bottom');
-    document.getElementById('email-in-use').classList.add('d-none');
+    window.location.href = "../html/sign_up.html";
 }
-
-
-function removeValueSignUpInput() {
-    let name = document.getElementById('name');
-    let email = document.getElementById('email');
-    let password = document.getElementById('password');
-
-    name.value = '';
-    email.value = '';
-    password.value = '';
-}
-
-
-function defaultViewSignUp() {
-    document.getElementById('submit-button').classList.add('d-none');
-    document.getElementById('no-submit-button').classList.remove('d-none');
-    document.getElementById('password').classList.remove('no-margin-bottom');
-    document.getElementById('successful-registration').classList.add('d-none');
-}
-
-
-function loginDisappear() {
-    document.getElementById('login-field').classList.add('d-none');
-    document.getElementById('sign-up-field').classList.add('d-none');
-    document.getElementById('register-field').classList.remove('d-none');
-}
-
-
 
 function showLogin() {
-    document.getElementById('login-field').classList.remove('d-none');
-    document.getElementById('sign-up-field').classList.remove('d-none');
-    document.getElementById('register-field').classList.add('d-none');
+    window.location.href = "../html/login.html";
+}
+
+function guestLogin() {
+    window.location.href = "../html/summary.html";
 }
 
 
@@ -51,7 +21,7 @@ function successfulRegistration() {
 
     users.push({ name: name.value, email: email.value, password: password.value });
 
-    document.getElementById('password').classList.add('no-margin-bottom');
+    document.getElementById('overlay').classList.remove('d-none');
     document.getElementById('successful-registration').classList.remove('d-none');
     console.log(users);
 }
@@ -65,11 +35,6 @@ function proofLogin() {
     if (user) {
         window.location.href = "../html/summary.html";
     }
-}
-
-
-function guestLogin() {
-    window.location.href = "../html/summary.html";
 }
 
 
