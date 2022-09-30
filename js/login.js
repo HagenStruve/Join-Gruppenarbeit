dcdaf02a170cbc4298ee41a5f90e8fcdfc97231b
matthiasmulzet <mulzetmatthias@gmail.com>
2022-09-30 22:34:25 +0200

    temporarily finished groundfunctions login

--- js/login.js
+++ js/login.js
@@ -1,5 +1,11 @@
+
+// after the successful registration, name, e-mail and password will be saved in this array
 let users = [];
 
+
+/**
+ * load the URL and then the registered users from the backend
+ */
 async function initLogin() {
     setURL("https://gruppe-313.developerakademie.net/Join/smallest_backend_ever-master");
     await downloadFromServer();
@@ -7,29 +13,36 @@ async function initLogin() {
 }
 
 
-
 function showSignUp() {
     window.location.href = "../html/sign_up.html";
 }
 
+
 function showLogin() {
     window.location.href = "../html/login.html";
 }
+
 
 function guestLogin() {
     window.location.href = "../html/summary.html";
 }
 
 
+/**
+ * after successful registration overlay over the whole page appears and a container which says that the registration was successful
+ * a div have the id=succesful-registration, this div includes a button. When you click the button you will be forwarded to the login page
+ */
 function successfulRegistration() {
     addUser();
-
     document.getElementById('overlay').classList.remove('d-none');
     document.getElementById('successful-registration').classList.remove('d-none');
-    console.log(users);
 }
 
 
+/**
+* pushes the value from the registration input fields in the users array
+* array will be saved as a JSON in the backend
+*/
 function addUser() {
     let name = document.getElementById('name');
     let email = document.getElementById('email');
@@ -41,6 +54,12 @@ function addUser() {
 }
 
 
+/**
+ * when you click on the Login Button, this function will be carried out
+ * function searches the email and the password from the login input fields in the users array
+ * if the email and password are in the array, login was succesful
+ * else a div appears that tell you, that the login dates are wrong
+ */
 function proofLogin() {
     let loginEmail = document.getElementById('login-email');
     let loginPassword = document.getElementById('login-password');
@@ -57,6 +76,11 @@ function proofLogin() {
 }
 
 
+/**
+ * onkeyup function
+ * function set the div with the information about wrong login dates on display:none,
+ * when the input email or the input password field are cleared by the user
+ */
 function proofInputLogin() {
     let loginEmail = document.getElementById('login-email');
     let loginPassword = document.getElementById('login-password');
@@ -68,6 +92,9 @@ function proofInputLogin() {
 }
 
 
+/**
+ * onkeyup function
+ */
 function proofInputSignUp() {
     let name = document.getElementById('name');
     let email = document.getElementById('email');
@@ -76,22 +103,47 @@ function proofInputSignUp() {
     let user = users.find(u => u.email == email.value);
 
     if (user) {
-        document.getElementById('password').classList.add('no-margin-bottom');
-        document.getElementById('email-in-use').classList.remove('d-none');
-        document.getElementById('submit-button').classList.add('d-none');
-        document.getElementById('no-submit-button').classList.remove('d-none');
+        showEmailInUseWarning();
     }
 
     else if (name.value.length >= 1 && email.value.length >= 1 && password.value.length >= 1) {
-        document.getElementById('submit-button').classList.remove('d-none');
-        document.getElementById('no-submit-button').classList.add('d-none');
+        showSubmitButton();
     }
 
     if (!user) {
-        document.getElementById('password').classList.remove('no-margin-bottom');
-        document.getElementById('email-in-use').classList.add('d-none');
+        removeEmailInUseWarning();
     }
 }
 
 
+/**
+ * if you write an email in the registration email input field which already exists in the users array,
+ * div appears which tells you that the email is already in use
+ */
+function showEmailInUseWarning() {
+    document.getElementById('password').classList.add('no-margin-bottom');
+    document.getElementById('email-in-use').classList.remove('d-none');
+    document.getElementById('submit-button').classList.add('d-none');
+    document.getElementById('no-submit-button').classList.remove('d-none');
+}
+
+
+/**
+ * if the warning div, that the mail in the registration email input field is already in use, will be showed
+ * this array removes the warning div if the user change the email in the input field
+ */
+function removeEmailInUseWarning() {
+    document.getElementById('password').classList.remove('no-margin-bottom');
+    document.getElementById('email-in-use').classList.add('d-none');
+}
+
+
+/**
+ * if every input field in the sign up container includes min. 1 symbol, the submit button will be showed
+ * before that a span, which looks almost the same as the submit button, will be showed and you can't click on that span
+ */
+function showSubmitButton() {
+    document.getElementById('submit-button').classList.remove('d-none');
+    document.getElementById('no-submit-button').classList.add('d-none');
+}
 
