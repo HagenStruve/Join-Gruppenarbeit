dae91cb0eb74a6b989858d23e23115f67c774dcb
HagenStruve <Struve94@web.de>
2022-10-03 15:42:33 +0200

    added contacts add contact first js

--- js/contact.js
+++ js/contact.js
@@ -1,1 +1,21 @@
-function newConatct() {}
+let contact = [];
+
+function newConatct() {
+    document.getElementById('edit-contact').classList.remove('d-none');
+    document.getElementById('edit-contact').classList.add('edit-contact');
+}
+
+
+function cancel() {
+    document.getElementById('edit-contact').classList.remove('edit-contact');
+    document.getElementById('edit-contact').classList.add('d-none');
+    document.getElementById('name').value = "";
+    document.getElementById('email').value = "";
+    document.getElementById('phone').value = "";
+}
+
+function addContact() {
+    document.getElementById('name').value;
+    document.getElementById('email').value;
+    document.getElementById('phone').value;
+}
